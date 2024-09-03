3359a7b3e09e2ae5c1f8436b82a2e6ff506c990f
JiinSeok <seokjiin1073@gmail.com>
2024-09-04 01:53:23 +0900

    [fix/#31] 프로젝트 config 오류 해결

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
-import path from 'path';
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-export default {
-  experimental: {
-    appDir: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve('./src');
-    return config;
-  },
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-};
+export default nextConfig;
