53d6f9e1fa1d120d7fa1cf432bd5d6d3ce391666
gayeong0916 <gy2039587@naver.com>
2024-09-03 18:59:49 +0900

    [feat/#14] 폼 만들기 버튼, 그 외의 버튼 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,6 @@ export default function Home() {
           </a>
         </div>
       </div>
-
       <div className={styles.center}>
         <Image
           className={styles.logo}
@@ -39,7 +38,6 @@ export default function Home() {
           priority
         />
       </div>
-
       <div className={styles.grid}>
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
---
+++ src/components/FloatingButton/OtherButton/OtherButton.tsx
@@ -0,0 +1,75 @@
+//컴포넌트 immport
+import Image from 'next/image';
+
+import BookmarkDisabled from '../../../../public/icons/ic-bookmark-fill.svg';
+import Bookmark from '../../../../public/icons/ic-bookmark.svg';
+import WritingForm from '../../../../public/icons/ic-edit2.svg';
+import Share from '../../../../public/icons/ic-share2.svg';
+import styles from './OtherButton.module.scss';
+
+/** 함수 타입 지정 */
+interface Props {
+  size?: 'small' | 'medium';
+  onClick?: () => void;
+  mode?: 'edit' | 'bookmark' | 'bookmarkDisabled' | 'share';
+}
+
+/** FloatingButton 컴포넌트 생성
+ * 글(알바토크) 생성, 북마크, 공유 버튼
+ */
+const OtherButton = ({ size = 'medium', onClick, mode = 'edit' }: Props) => {
+  let buttonClass = styles.OtherButton;
+
+  if (size === 'small') buttonClass += ` ${styles.small}`;
+
+  const renderMode = {
+    edit: (
+      <button className={buttonClass} onClick={onClick}>
+        <Image
+          src={WritingForm}
+          className={styles.otherbuttonImg}
+          alt="글 작성하기"
+          width="36"
+          height="36"
+        />
+      </button>
+    ),
+    bookmark: (
+      <button className={styles.bookmark} onClick={onClick}>
+        <Image
+          src={Bookmark}
+          className={styles.otherbuttonImg}
+          alt="북마크"
+          width="36"
+          height="36"
+        />
+      </button>
+    ),
+    bookmarkDisabled: (
+      <button className={styles.bookmark} onClick={onClick}>
+        <Image
+          src={BookmarkDisabled}
+          className={styles.otherbuttonImg}
+          alt="북마크 취소"
+          width="36"
+          height="36"
+        />
+      </button>
+    ),
+    share: (
+      <button className={buttonClass} onClick={onClick}>
+        <Image
+          src={Share}
+          className={styles.otherbuttonImg}
+          alt="공유"
+          width="36"
+          height="36"
+        />
+      </button>
+    ),
+  };
+
+  return <>{renderMode[mode]}</>;
+};
+
+export default FloatingButton;
