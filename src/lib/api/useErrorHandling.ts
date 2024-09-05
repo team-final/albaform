bc7fd4555f808640f555f7f4f1c00afe67919372
JiinSeok <seokjiin1073@gmail.com>
2024-09-05 14:26:20 +0900

    [chore/#20] 네트워크 에러 처리 훅 추가

--- src/lib/api/useErrorHandling.ts
+++ src/lib/api/useErrorHandling.ts
@@ -1,23 +1,37 @@
-// 에러 처리를 위한 커스텀 훅을 작성합니다.
+// import { useToast } from '@chakra-ui/react';
+import { AxiosError } from 'axios';
+import { useCallback } from 'react';
 
-// import { useToast } from '@chakra-ui/react'; // 예시로 Chakra UI의 Toast 사용
-// import { useCallback } from 'react';
-//
-// const useErrorHandling = () => {
-//   const toast = useToast();
-//
-//   const handleError = useCallback((error: any) => {
-//     console.error('API 호출 에러:', error.message);
-//     toast({
-//       title: "오류 발생",
-//       description: error.message,
-//       status: "error",
-//       duration: 3000,
-//       isClosable: true,
-//     });
-//   }, [toast]);
-//
-//   return { handleError };
-// };
-//
-// export default useErrorHandling;
+const useErrorHandling = () => {
+  // const toast = useToast();
+
+  /**
+   * @todo 상태코드에 따라 리다이렉트 등 예외처리 추가
+   * @todo 서비스 공통 모달 적용
+   */
+  const handleError = useCallback(
+    (error: AxiosError, customError: { title?: string; message?: string }) => {
+      console.error(
+        'API 호출 에러:',
+        error.response?.data,
+        error.message,
+        new Date().toISOString(),
+      );
+      alert(
+        `${customError.title || '에러가 발생했습니다.'}\n${customError.message || ''}${error.response?.data}\n${error.message}`,
+      );
+      // toast({
+      //   title: '오류 발생',
+      //   description: error.message,
+      //   status: 'error',
+      //   duration: 3000,
+      //   isClosable: true,
+      // });
+    },
+    [],
+  );
+
+  return { handleError };
+};
+
+export default useErrorHandling;
