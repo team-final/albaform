026cfc00c5fe40489e27fe0ffac4d42ef0f73535
JuhyeokC <bos2ablues@gmail.com>
2024-09-06 14:04:28 +0900

    [feat/#4] RootHeader 컴포넌트 추가

--- src/components/RootHeader/RootHeader.tsx
+++ src/components/RootHeader/RootHeader.tsx
@@ -1,31 +1,30 @@
-'use client';
+'use client'
 
-import { useState } from 'react';
+import { useState } from 'react'
 
-import AuthSpecificMenu from '../AuthSpecificMenu/AuthSpecificMenu';
-import GNB from '../GNB/GNB';
-import HamburgerButton from '../HamburgerButton/HamburgerButton';
-import RootLogo from '../RootLogo/RootLogo';
-import RootNav from '../RootNav/RootNav';
-import styles from './RootHeader.module.scss';
+import AuthSpecificMenu from '../AuthSpecificMenu/AuthSpecificMenu'
+import HamburgerButton from '../HamburgerButton/HamburgerButton'
+import RootLogo from '../RootLogo/RootLogo'
+import RootNav from '../RootNav/RootNav'
+import styles from './RootHeader.module.scss'
 
-type pageTypes = 'landing' | 'auth' | 'content';
+type pageTypes = 'landing' | 'auth' | 'content'
 
 const RootHeader = () => {
   /**
    * @todo
    * 1. Zustand 로 페이지 타입 가져오기 [ landing, auth, content ];
    */
-  const [showGNB, setShowGNB] = useState<boolean>(false);
+  const [showGNB, setShowGNB] = useState<boolean>(false)
 
-  const type: pageTypes[] = ['landing', 'auth', 'content'];
+  const type: pageTypes[] = ['landing', 'auth', 'content']
 
-  const router: pageTypes = type[0];
+  const router: pageTypes = type[0]
 
   return (
     <>
-      <header className={styles['header']}>
-        <div className={styles['inner']}>
+      <header className={styles.header}>
+        <div className={styles.inner}>
           <RootLogo />
           {router === 'auth' ? (
             <AuthSpecificMenu />
@@ -37,9 +36,9 @@ const RootHeader = () => {
           )}
         </div>
       </header>
-      {showGNB && <GNB closeAction={() => setShowGNB(false)} />}
+      {showGNB && <p>GNB 컴포넌트</p>}
     </>
-  );
-};
+  )
+}
 
-export default RootHeader;
+export default RootHeader
