aa2ce594557c3b48ee328d9bad4f10e738415957
JuhyeokC <bos2ablues@gmail.com>
2024-09-06 13:35:13 +0900

    [feat/#4] RootNav 컴포넌트 개발

---
+++ src/components/RootNav/RootNav.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+import styles from './RootNav.module.scss';
+
+const RootNav = () => {
+  return (
+    <div className={styles.nav}>
+      <Link href="/" draggable="false" className={styles.active}>
+        알바 목록
+      </Link>
+      <Link href="/" draggable="false">
+        알바토크
+      </Link>
+      <Link href="/" draggable="false">
+        내 알바폼
+      </Link>
+    </div>
+  );
+};
+
+export default RootNav;
