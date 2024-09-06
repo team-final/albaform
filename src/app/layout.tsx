0eb9c0831dcf42f60a32d1425ef7a3d485b13565
JuhyeokC <bos2ablues@gmail.com>
2024-09-06 13:45:55 +0900

    [feat/#4] 기본 언어 한국어 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body className={inter.className}>
         <RootHeader />
         {children}
