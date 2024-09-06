491d801e67f12c4a444127fd254acff87ee045ce
JuhyeokC <bos2ablues@gmail.com>
2024-09-06 13:42:35 +0900

    [feat/#4] XButton 컴포넌트 개발

---
+++ src/components/XButton/XButton.tsx
@@ -0,0 +1,17 @@
+import PlainButton from '../PlainButton/PlainButton';
+import styles from './XButton.module.scss';
+import IconX from '/public/icons/ic-X.svg';
+
+interface XButtonProps {
+  onClick?: () => void;
+}
+
+const XButton = ({ onClick }: XButtonProps) => {
+  return (
+    <PlainButton onClick={onClick}>
+      <IconX className={styles.icon} />
+    </PlainButton>
+  );
+};
+
+export default XButton;
