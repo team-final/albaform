ba893d5ac1690a6674101383b0aff80b049ad8a6
JuhyeokC <bos2ablues@gmail.com>
2024-09-06 17:52:13 +0900

    [feat/#4] 메타데이터 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import React from 'react';
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import React from 'react'
 
-import RootHeader from '../components/RootHeader/RootHeader';
-import '../styles/globals.css';
+import RootHeader from '../components/RootHeader/RootHeader'
+import '../styles/globals.css'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'albaform - 한 곳에서 관리하는 알바 구인 플랫폼',
   description: '한 곳에서 관리하는 알바 구인 플랫폼',
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="ko">
@@ -24,5 +24,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-  );
+  )
 }
