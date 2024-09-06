96509377d9ed71cdb1debae125538cb2a12eb269
JuhyeokC <bos2ablues@gmail.com>
2024-09-06 17:50:36 +0900

    [feat/#4] LogoutButton 컴포넌트 개발

--- src/components/LogoutButton/LogoutButton.tsx
+++ src/components/LogoutButton/LogoutButton.tsx
@@ -1,22 +1,22 @@
-import PlainButton from '../PlainButton/PlainButton';
-import styles from './LogoutButton.module.scss';
-import IconLogout from '/public/icons/ic-logout.svg';
+import PlainButton from '../PlainButton/PlainButton'
+import styles from './LogoutButton.module.scss'
+import IconLogout from '/public/icons/ic-logout.svg'
 
 interface LogoutButtonProps {
-  className?: string;
+  className?: string
 }
 
 const LogoutButton = ({ className = '' }: LogoutButtonProps) => {
   const handleLogout = () => {
-    console.log('LOGOUT');
-  };
+    console.log('LOGOUT')
+  }
 
   return (
     <PlainButton className={className} onClick={handleLogout}>
       <IconLogout className={styles.icon} />
       <span>로그아웃</span>
     </PlainButton>
-  );
-};
+  )
+}
 
-export default LogoutButton;
+export default LogoutButton
