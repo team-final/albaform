a388afb741425d7bb76e4b823f69227c5306c285
JuhyeokC <bos2ablues@gmail.com>
2024-09-10 15:03:18 +0900

    [feat/#5] 기존 폰트 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import React from 'react'
 
 import RootHeader from '../components/RootHeader/RootHeader'
 import '../styles/globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'albaform - 한 곳에서 관리하는 알바 구인 플랫폼',
@@ -19,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>
+      <body>
         <RootHeader />
         {children}
       </body>
