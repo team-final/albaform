edefd0b04e21bf72a31dba71ae4ae6e66de6da4c
JiinSeok <seokjiin1073@gmail.com>
2024-09-12 00:56:05 +0900

    [fix/#53] QueryClientProvider 있는 루트 레이아웃에 use client 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
+'use client'
+
 import queryClient from '@/lib/queries/queryClient'
-import type { Metadata } from 'next'
+// import type { Metadata } from 'next'
 import React from 'react'
 import { QueryClientProvider } from 'react-query'
 
 import RootHeader from '../components/RootHeader/RootHeader'
 import '../styles/globals.css'
 
-export const metadata: Metadata = {
-  title: 'albaform - 한 곳에서 관리하는 알바 구인 플랫폼',
-  description: '한 곳에서 관리하는 알바 구인 플랫폼',
-}
+// export const metadata: Metadata = {
+//   title: 'albaform - 한 곳에서 관리하는 알바 구인 플랫폼',
+//   description: '한 곳에서 관리하는 알바 구인 플랫폼',
+// }
 
 export default function RootLayout({
   children,
