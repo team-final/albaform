5f439127421c7900c1ea334dcd800dc72516235c
JuhyeokC <bos2ablues@gmail.com>
2024-09-11 17:49:09 +0900

    [feat/#5] 랜딩페이지 퍼블리싱

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,88 @@
 import Image from 'next/image'
 
 import styles from './page.module.scss'
-import Image01 from '/public/images/landing/md/01.png'
+import Image02lg from '/public/images/landing/lg/02.png'
+import Image03lg from '/public/images/landing/lg/03.png'
+import Image04lg from '/public/images/landing/lg/04.png'
+import Image05lg from '/public/images/landing/lg/05.png'
+import Image01md from '/public/images/landing/md/01.png'
+import Image02md from '/public/images/landing/md/02.png'
+import Image03md from '/public/images/landing/md/03.png'
+import Image04md from '/public/images/landing/md/04.png'
+import Image05md from '/public/images/landing/md/05.png'
+import Image02sm from '/public/images/landing/sm/02.png'
+import Image03sm from '/public/images/landing/sm/03.png'
+import Image04sm from '/public/images/landing/sm/04.png'
+import Image05sm from '/public/images/landing/sm/05.png'
 import LogoText from '/public/images/logo/logo-text.svg'
+
+const IMAGES = [
+  [Image02lg, Image03lg, Image04lg, Image05lg],
+  [Image02md, Image03md, Image04md, Image05md],
+  [Image02sm, Image03sm, Image04sm, Image05sm],
+]
+
+const IMAGE_DESC = [
+  '어디서든 지원받으세요. 다양한 사이트, SNS, 문자까지 언제 어디서든 알바생을 구해보세요.',
+  '쉽고 빨라요. 1분만에 알바폼을 만들어 보세요! 링크를 복사하여 어디서든지 사용하세요.',
+  '한 곳에서 쉽게 관리하세요. 알바폼 관리 페이지에서 지원 현황을 확인하고 지원자별 상태를 관리할 수 있습니다.',
+  '쉽고 빠르게 알바 지원. 간단한 정보만 입력해도 알바 지원이 가능합니다.',
+]
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <section className={styles['main-visual']}>
+      <section className={styles.visual}>
         <LogoText />
         <h1>한 곳에서 관리하는 알바 구인 플랫폼</h1>
-        <button>알바폼 시작하기</button>
+        <button className={styles.button}>알바폼 시작하기</button>
         <div>
           <Image
             fill
-            src={Image01}
+            src={Image01md}
             alt="albaform 서류 뭉치 이미지"
             style={{ objectFit: 'contain' }}
             draggable="false"
           />
+        </div>
+      </section>
+      {/**
+       * @todo
+       * Next/Image 컴포넌트 priority & sizez 경고 해결하기
+       */}
+      <section className={styles.promotion}>
+        {IMAGES.map((items, sizes) => {
+          return (
+            <div
+              key={`promotion-image-size-${sizes}`}
+              className={styles.container}
+            >
+              {items.map((image, index) => {
+                return (
+                  <div
+                    key={`promotion-image-${sizes}-${index}`}
+                    className={styles.image}
+                  >
+                    <Image
+                      fill
+                      src={image}
+                      alt={IMAGE_DESC[index]}
+                      style={{ objectFit: 'contain' }}
+                      draggable="false"
+                    />
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })}
+        <div className={styles.foot}>
+          <h1>
+            한 곳에서 관리하는
+            <br />
+            알바 구인 플랫폼
+          </h1>
+          <button className={styles.button}>알바폼 시작하기</button>
         </div>
       </section>
     </main>
