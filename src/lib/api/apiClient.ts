16593d61574c8dde0a25525cc859a9ae08a17be8
JiinSeok <seokjiin1073@gmail.com>
2024-09-12 01:42:50 +0900

    [fix/#53] 네트워크 함수 모듈 삭제 (리액트 쿼리로 대체 대비)

--- src/lib/api/apiClient.ts
+++ src/lib/api/apiClient.ts
@@ -1,6 +1,4 @@
-import handleError from '@/lib/api/errorHandler'
-import { CustomMessage } from '@/lib/types/types'
-import axios, { AxiosError, AxiosInstance } from 'axios'
+import axios, { AxiosInstance } from 'axios'
 
 const apiClient: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
@@ -8,83 +6,5 @@ const apiClient: AxiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
 })
-
-export const fetchData = async (
-  endpoint: string,
-  customMessage: CustomMessage,
-) => {
-  try {
-    const response = await apiClient.get(endpoint)
-    return response.data
-  } catch (error: unknown) {
-    if (error instanceof AxiosError) {
-      handleError(error, customMessage)
-    }
-    console.error('Unexpected error:', error)
-  }
-}
-
-export const postData = async (
-  endpoint: string,
-  data: unknown,
-  customMessage: CustomMessage,
-) => {
-  try {
-    const response = await apiClient.post(endpoint, data)
-    return response.data
-  } catch (error: unknown) {
-    if (error instanceof AxiosError) {
-      handleError(error, customMessage)
-    }
-    console.error('Unexpected error:', error)
-  }
-}
-
-export const updateData = async (
-  endpoint: string,
-  data: unknown,
-  customMessage: CustomMessage,
-) => {
-  try {
-    const response = await apiClient.put(endpoint, data)
-    return response.data
-  } catch (error: unknown) {
-    if (error instanceof AxiosError) {
-      handleError(error, customMessage)
-    }
-    console.error('Unexpected error:', error)
-  }
-}
-
-export const patchData = async (
-  endpoint: string,
-  data: unknown,
-  customMessage: CustomMessage,
-) => {
-  try {
-    const response = await apiClient.patch(endpoint, data)
-    return response.data
-  } catch (error: unknown) {
-    if (error instanceof AxiosError) {
-      handleError(error, customMessage)
-    }
-    console.error('Unexpected error:', error)
-  }
-}
-
-export const deleteData = async (
-  endpoint: string,
-  customMessage: CustomMessage,
-) => {
-  try {
-    const response = await apiClient.delete(endpoint)
-    return response.data
-  } catch (error: unknown) {
-    if (error instanceof AxiosError) {
-      handleError(error, customMessage)
-    }
-    console.error('Unexpected error:', error)
-  }
-}
 
 export default apiClient
