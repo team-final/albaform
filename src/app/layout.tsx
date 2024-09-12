e51de20df6139d772ad9773f54f6eeaea109a1fb
JiinSeok <seokjiin1073@gmail.com>
2024-09-12 12:12:49 +0900

    [feat/#53] 루트레이아웃에서 QueryClientProvider 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,13 @@
-'use client'
-
-import queryClient from '@/lib/queries/queryClient'
-// import type { Metadata } from 'next'
+import type { Metadata } from 'next'
 import React from 'react'
-import { QueryClientProvider } from 'react-query'
 
 import RootHeader from '../components/RootHeader/RootHeader'
 import '../styles/globals.css'
 
-// export const metadata: Metadata = {
-//   title: 'albaform - 한 곳에서 관리하는 알바 구인 플랫폼',
-//   description: '한 곳에서 관리하는 알바 구인 플랫폼',
-// }
+export const metadata: Metadata = {
+  title: 'albaform - 한 곳에서 관리하는 알바 구인 플랫폼',
+  description: '한 곳에서 관리하는 알바 구인 플랫폼',
+}
 
 export default function RootLayout({
   children,
@@ -19,13 +15,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <html lang="ko">
-        <body>
-          <RootHeader />
-          {children}
-        </body>
-      </html>
-    </QueryClientProvider>
+    <html lang="ko">
+      <body>
+        <RootHeader />
+        {children}
+      </body>
+    </html>
   )
 }
