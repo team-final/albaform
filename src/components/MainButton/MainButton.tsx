3693ab2d99ea8ec2a632c05a6676f152be6e803e
yoooonbong <a1056719@gmail.com>
2024-09-13 20:00:30 +0900

    [feat/#71] MainButton 컴포넌트 개발 (#72)

---
+++ src/components/MainButton/MainButton.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import style from '@/components/BasicButton/BasicButton.module.scss'
+import Image from 'next/image'
+import React from 'react'
+
+interface ButtonProps {
+  type: 'solid' | 'outline'
+  disabled: boolean
+  onClick: () => void
+  children: React.ReactNode
+}
+
+interface IconProps {
+  src: string
+  altText?: string
+}
+
+interface TextProps {
+  children: React.ReactNode
+}
+
+const MainButton = ({ type, disabled, onClick, children }: ButtonProps) => {
+  const buttonClass = `${style.default} ${style[type]}`
+
+  return (
+    <button className={buttonClass} disabled={disabled} onClick={onClick}>
+      {children}
+    </button>
+  )
+}
+
+const ButtonIcon = ({ src, altText = '아이콘' }: IconProps) => {
+  return (
+    <Image
+      src={src}
+      alt={altText}
+      width={36}
+      height={36}
+      className={style.icon}
+    />
+  )
+}
+
+const ButtonText = ({ children }: TextProps) => {
+  return <span>{children}</span>
+}
+
+MainButton.Icon = ButtonIcon
+MainButton.Text = ButtonText
+
+export default MainButton
