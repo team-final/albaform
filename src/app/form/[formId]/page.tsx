9628944a3ab32f822ce39fc2b95e1a2dbf2f66f8
gayeong0916 <gy2039587@naver.com>
2024-09-14 13:37:50 +0900

    [feat/#63] 버튼 추가

--- src/app/form/[formId]/page.tsx
+++ src/app/form/[formId]/page.tsx
@@ -1,16 +1,22 @@
-import ApplicationStatus from '@/components/FormDetails/ApplicationStatus/ApplicationStatus'
+'use client'
+
 import ContactInfo from '@/components/FormDetails/ContactInfo/ContactInfo'
 import FormDetailsInfo from '@/components/FormDetails/FormDetailsInfo/FormDetailsInfo'
 import Location from '@/components/FormDetails/Location/Location'
 import Requirements from '@/components/FormDetails/Requirements/Requirements'
 import WorkScheduleInfo from '@/components/FormDetails/WorkScheduleInfo/WorkScheduleInfo'
+import MainButton from '@/components/MainButton/MainButton'
 import Image from 'next/image'
 
 import styles from './page.module.scss'
 
 export default function FormDetailsPage() {
+  const handleApplyClick = () => {
+    console.log('클릭')
+  }
+
   return (
-    <div className={styles['job-details']}>
+    <main className={styles['job-details']}>
       <Image
         src="/icons/ic-plus.svg"
         alt="기본 이미지"
@@ -20,30 +26,42 @@ export default function FormDetailsPage() {
       />
       <div className={styles['job-details-container']}>
         <div className={styles['job-details-content']}>
-          <div className={styles['job-details-info']}>
+          <section className={styles['job-details-info']}>
             <FormDetailsInfo />
-          </div>
-          <div className={styles['schedule-contact-container']}>
+          </section>
+          <section className={styles['schedule-contact-container']}>
             <WorkScheduleInfo />
             <ContactInfo />
-            <ApplicationStatus />
-          </div>
+          </section>
         </div>
 
         <div className={styles['location-requirements-container']}>
-          <div className={styles['location-info']}>
+          <section className={styles['location-info']}>
             <Location />
-          </div>
-          <div className={styles['requirements-info']}>
+          </section>
+          <section className={styles['requirements-info']}>
             <Requirements />
-          </div>
+          </section>
         </div>
 
         <div className={styles['button-container']}>
-          <button>지원하기</button>
-          <button>내 지원 내역 보기</button>
+          <MainButton type="solid" disabled={false} onClick={handleApplyClick}>
+            <MainButton.Icon src="/icons/ic-writing.svg" altText="지원하기" />
+            <MainButton.Text>지원하기</MainButton.Text>
+          </MainButton>
+          <MainButton
+            type="outline"
+            disabled={false}
+            onClick={handleApplyClick}
+          >
+            <MainButton.Icon
+              src="/icons/ic-apply-list.svg"
+              altText="지원하기"
+            />
+            <MainButton.Text>내 지원내역 보기</MainButton.Text>
+          </MainButton>
         </div>
       </div>
-    </div>
+    </main>
   )
 }
