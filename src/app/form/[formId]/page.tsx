aab7ac8a4809c5fdafb57875bb4af569e50420fa
gayeong0916 <gy2039587@naver.com>
2024-09-16 20:22:26 +0900

    [feat/#63] 정적 생성 환경변수 테스트

--- src/app/form/[formId]/page.tsx
+++ src/app/form/[formId]/page.tsx
@@ -1,24 +1,27 @@
 import FormDetailsClient from '@/components/FormDetails/FormDetailsClient/FormDetailsClient'
+import { getFormLists } from '@/lib/api/formDetails'
 
 import styles from './page.module.scss'
 
 // 정적 생성 함수
-// export const generateStaticParams = async () => {
-//   const formLists = await getFormLists()
-//   const formIds = formLists.data.map((form: { id: number }) => ({
-//     id: form.id.toString(),
-//   }))
-//   return formIds.map((form: { id: number }) => ({
-//     params: {
-//       id: form.id,
-//     },
-//   }))
-// }
+export const generateStaticParams = async () => {
+  const LIMIT = 10
+  const formLists = await getFormLists(LIMIT)
+  const formIds = formLists.data.map((form: { id: number }) => ({
+    id: form.id.toString(),
+  }))
+  return formIds.map((form: { id: number }) => ({
+    params: {
+      id: form.id,
+    },
+  }))
+}
 
-const FormDetailsPage = async () => {
+const FormDetailsPage = async ({ params }: { params: { formId: string } }) => {
+  const { formId } = params
   return (
     <main className={styles['job-details']}>
-      <FormDetailsClient formId={1} />
+      <FormDetailsClient formId={Number(formId)} />
     </main>
   )
 }
