bec91c40b81e7d8082b4f6833e074d78558f0e81
JiinSeok <seokjiin1073@gmail.com>
2024-09-24 14:55:59 +0900

    [feat/#65] 메시지 상수 추가

--- src/lib/data/constants.ts
+++ src/lib/data/constants.ts
@@ -1,1 +1,21 @@
-export {}
+export const DEFAULT_ERROR_MESSAGE = {
+  title: '오류',
+}
+
+export const SIGNIN_ERROR_MESSAGE = {
+  title: '로그인 실패',
+}
+
+export const AUTH_USER_ERROR_MESSAGE = {
+  title: '사용자 로드 실패',
+}
+
+export const TEST_ID_APPLICANT = {
+  email: 'testApplicant@email.com',
+  password: '00000000',
+}
+
+export const TEST_ID_OWNER = {
+  email: 'testOwner@email.com',
+  password: '00000000',
+}
