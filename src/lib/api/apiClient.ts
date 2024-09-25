8f1ebe2481ac3f3145c191412fab419f29f58a36
gayeong0916 <gy2039587@naver.com>
2024-09-25 15:11:38 +0900

    [feat/#63] 임시 토큰 제거

--- src/lib/api/apiClient.ts
+++ src/lib/api/apiClient.ts
@@ -1,13 +1,9 @@
 import axios, { AxiosInstance } from 'axios'
 
-const accessToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwic2NvcGUiOiJhY2Nlc3MiLCJpYXQiOjE3MjcyNDM0OTcsImV4cCI6MTcyNzI1MDY5N30.eRvwzpzJzw2TlKF3eQUZaF8qkWDHOhcpZjOBltQZhQk'
-
 const apiClient: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${accessToken}`,
   },
 })
 
