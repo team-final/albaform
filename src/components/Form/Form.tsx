bde9f2502222e935c7456b6d686c8573eff405c9
JuhyeokC <bos2ablues@gmail.com>
2024-09-26 01:30:11 +0900

    [feat/#94] Form 컴포넌트 구조 수정

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -6,7 +6,7 @@ import {
   LabelProps,
 } from '@/lib/types/types'
 import classNames from 'classnames'
-import { createContext, useContext } from 'react'
+import { ReactNode, createContext, useContext, useState } from 'react'
 import {
   FieldErrors,
   FieldValues,
@@ -15,7 +15,10 @@ import {
   useForm,
 } from 'react-hook-form'
 
+import styles from './Form.module.scss'
+
 interface FormContextProps {
+  formId: string
   register: UseFormRegister<FieldValues>
   errors: FieldErrors
   onSubmit: (data: FieldValues) => void
@@ -49,7 +52,7 @@ export default function Form({
   onSubmit,
   initialValues,
 }: FormProps) {
-  const FormClass = classNames(className, 'form')
+  const FormClass = classNames(styles.form, className)
   const {
     register,
     handleSubmit,
@@ -57,7 +60,7 @@ export default function Form({
   } = useForm({ defaultValues: initialValues })
 
   return (
-    <FormContext.Provider value={{ register, errors, onSubmit }}>
+    <FormContext.Provider value={{ formId, register, errors, onSubmit }}>
       <form id={formId} onSubmit={handleSubmit(onSubmit)} className={FormClass}>
         {children}
       </form>
@@ -66,8 +69,34 @@ export default function Form({
 }
 
 function Title({ children, className }: ComponentProps) {
-  const FormTitleClass = classNames(className, 'form-title')
-  return <h2 className={FormTitleClass}>{children}</h2>
+  const FormTitleClass = classNames(styles['form-title'], className)
+  return <header className={FormTitleClass}>{children}</header>
+}
+
+function Fieldset({ children, className }: ComponentProps) {
+  const cn = classNames(styles['form-fieldset'], className)
+  return <section className={cn}>{children}</section>
+}
+
+function Legend({ children, className }: ComponentProps) {
+  const cn = classNames(styles['form-legend'], className)
+  return <p className={cn}>{children}</p>
+}
+
+interface LabelContextProps {
+  formId: string
+  labelName: string
+  labelIdx: number
+}
+
+const LabelContext = createContext<LabelContextProps | undefined>(undefined)
+
+export const useLabelContext = () => {
+  const context = useContext(LabelContext)
+  if (!context) {
+    throw new Error('useContext를 LabelProvider 안에서 사용하세요.')
+  }
+  return context
 }
 
 /**
@@ -76,22 +105,38 @@ function Title({ children, className }: ComponentProps) {
  * @param className
  * @param isInline - label과 input 한줄로
  */
-function Field({ children, className, isInline = false }: FormFieldProps) {
-  const FormFieldClass = classNames(className, 'form-field', {
-    'form-field-inline': isInline,
-    'form-field-block': !isInline,
+function Field({
+  children,
+  className,
+  htmlFor = '',
+  isInline = false,
+}: FormFieldProps) {
+  const { formId } = useFormContext()
+  const cn = classNames(styles['form-field'], className, {
+    [styles['form-field-inline']]: isInline,
+    [styles['form-field-block']]: !isInline,
   })
-  return <label className={FormFieldClass}>{children}</label>
+
+  const labelName = htmlFor.replace(/\b[a-z]/, (letter) => letter.toUpperCase()) // 첫 글자 대문자로
+  const labelIdx = parseInt(String(Math.random() * 100)) // 난수 생성
+
+  return (
+    <LabelContext.Provider value={{ formId, labelName, labelIdx }}>
+      <label className={cn} htmlFor={formId + labelName + labelIdx}>
+        {children}
+      </label>
+    </LabelContext.Provider>
+  )
 }
 
-function Label({ children, htmlFor, className }: LabelProps) {
-  const LabelClass = classNames(className)
-  return (
-    <label htmlFor={htmlFor} className={LabelClass}>
-      {' '}
-      {children}{' '}
-    </label>
-  )
+function Label({ children, className }: LabelProps) {
+  const LabelClass = classNames(styles['form-label'], className)
+  return <p className={LabelClass}>{children}</p>
+}
+
+function Wrap({ children, className }: ComponentProps) {
+  const cn = classNames(styles['form-wrap'], className)
+  return <div className={cn}>{children}</div>
 }
 
 /**
@@ -120,6 +165,7 @@ function Input({
   placeholder,
   autoComplete = 'off',
   pattern,
+  value,
   validate,
 }: InputProps) {
   const rules: RegisterOptions = {
@@ -129,26 +175,108 @@ function Input({
     pattern: pattern || undefined,
     validate,
   }
-  const { register, errors } = useFormContext()
+  const [visibility, setVisibility] = useState<boolean>(true)
+  const { formId, register, errors } = useFormContext()
+  const { labelName, labelIdx } = useLabelContext()
+  const cn = classNames(styles['form-input'], className)
+
+  const inputType =
+    type === 'password' ? (visibility ? 'password' : 'text') : type
+
   return (
     <>
       <input
         {...register(name, rules)}
-        className={className}
-        type={type}
+        className={cn}
+        type={inputType}
         placeholder={placeholder}
         autoComplete={autoComplete}
         checked={checked}
         disabled={disabled}
+        id={formId + labelName + labelIdx}
+        value={value}
       />
+      {type === 'password' && (
+        <VisibilityToggleButton
+          visibility={visibility}
+          handleToggle={() => {
+            setVisibility((prev) => !prev)
+          }}
+        />
+      )}
       {errors?.[name] && (
-        <span className={'input-error-message'}>잘못된 입력입니다.</span>
+        <p className={styles['input-error-message']}>잘못된 입력입니다.</p>
       )}
     </>
   )
 }
 
+function Textarea({
+  className,
+  name,
+  disabled = false,
+  required = false,
+  placeholder,
+  validate,
+}: InputProps) {
+  const rules: RegisterOptions = {
+    required,
+    validate,
+  }
+  const { formId, register, errors } = useFormContext()
+  const { labelName, labelIdx } = useLabelContext()
+  const cn = classNames(styles['form-input'], className)
+
+  return (
+    <>
+      <input
+        {...register(name, rules)}
+        className={cn}
+        placeholder={placeholder}
+        disabled={disabled}
+        id={formId + labelName + labelIdx}
+      />
+      {errors?.[name] && (
+        <p className={styles['input-error-message']}>잘못된 입력입니다.</p>
+      )}
+    </>
+  )
+}
+
+interface VisibilityToggleButtonProps {
+  visibility: boolean
+  handleToggle: () => void
+}
+
+function VisibilityToggleButton({
+  visibility,
+  handleToggle,
+}: VisibilityToggleButtonProps) {
+  return (
+    <button
+      type="button"
+      data-visibility={visibility}
+      onClick={handleToggle}
+      className={styles['visibility-toggle-button']}
+    ></button>
+  )
+}
+interface SubmitButtonProps {
+  buttonStyle: 'solid' | 'outline'
+  children: ReactNode
+}
+
+function SubmitButton({ buttonStyle, children }: SubmitButtonProps) {
+  console.log(buttonStyle)
+  console.log(children)
+}
+
 Form.Title = Title
+Form.Fieldset = Fieldset
+Form.Legend = Legend
 Form.Field = Field
 Form.Label = Label
+Form.Wrap = Wrap
 Form.Input = Input
+Form.Textarea = Textarea
+Form.SubmitButton = SubmitButton
