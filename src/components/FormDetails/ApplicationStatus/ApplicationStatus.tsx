1484110e4e2c4ebfe8dee5f436b8f803a8dd6f66
gayeong0916 <gy2039587@naver.com>
2024-09-28 00:41:00 +0900

    [feat/#104] 지원하기 tooltip

--- src/components/FormDetails/ApplicationStatus/ApplicationStatus.tsx
+++ src/components/FormDetails/ApplicationStatus/ApplicationStatus.tsx
@@ -1,6 +1,16 @@
+import Image from 'next/image'
+import { useState } from 'react'
+
 import styles from './ApplicationStatus.module.scss'
 
 const ApplicationStatus = () => {
+  const [isVisible, setIsVisible] = useState(true)
+
+  const handleTooltipCloseClick = () => {
+    setIsVisible(false)
+    console.log(isVisible)
+  }
+
   return (
     <section className={styles['application-status']}>
       <div
@@ -16,7 +26,46 @@ const ApplicationStatus = () => {
       <div
         className={`${styles['application-status-info']} ${styles['info-no-line']}`}
       >
-        <h3 className={styles['info-title']}>진행 상태</h3>
+        <div className={styles['application-status-button-container']}>
+          <h3 className={styles['info-title']}>진행 상태</h3>
+          <button className={styles['status-button']}>
+            <Image
+              src="/icons/ic-edit.svg"
+              alt="진행 상태 편집"
+              width={36}
+              height={36}
+            />
+          </button>
+
+          <div
+            className={`${styles['application-status-tooltip']} ${isVisible ? '' : styles['tooltip-isvisible-false']}`}
+          >
+            <div className={styles['application-status-tooltip-container']}>
+              <Image
+                src="/icons/ic-info.svg"
+                alt="정보"
+                width={36}
+                height={36}
+                className={styles['stauts-button-image']}
+              />
+              <span className={styles['application-status-tooltip-text']}>
+                알바폼 현재 진행상태를 변경할 수 있어요!
+              </span>
+            </div>
+            <button
+              className={styles['status-button']}
+              onClick={handleTooltipCloseClick}
+            >
+              <Image
+                src="/icons/ic-X.svg"
+                alt="닫기"
+                width={36}
+                height={36}
+                className={styles['stauts-button-image']}
+              />
+            </button>
+          </div>
+        </div>
         <p className={styles['info-content']}>면접 대기</p>
       </div>
     </section>
