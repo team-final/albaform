3f5b1190d29fd5bd4db327e517464dce9d9b5160
JuhyeokC <bos2ablues@gmail.com>
2024-10-03 17:01:16 +0900

    [feat/#145] DateRangePicker 컴포넌트 추가

---
+++ src/components/DateRangePicker/DateRangePicker.tsx
@@ -0,0 +1,64 @@
+import { ConfigProvider, DatePicker, DatePickerProps } from 'antd'
+import ko from 'antd/es/date-picker/locale/ko_KR'
+import koKR from 'antd/es/locale/ko_KR'
+import dayjs from 'dayjs'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+
+import styles from './DateRangePicker.module.scss'
+
+const buddhistLocale: typeof ko = {
+  ...ko,
+  lang: {
+    ...ko.lang,
+    fieldDateFormat: 'YYYY-MM-DD',
+    fieldDateTimeFormat: 'YYYY-MM-DD HH:mm:ss',
+    yearFormat: 'YYYY',
+    cellYearFormat: 'YYYY',
+  },
+}
+
+const globalBuddhistLocale: typeof koKR = {
+  ...koKR,
+  DatePicker: {
+    ...koKR.DatePicker!,
+    lang: buddhistLocale.lang,
+  },
+}
+
+dayjs.extend(customParseFormat)
+
+interface DateRangePickerProps {
+  startDate: string
+  endDate: string
+  setValue: (key: string, value: string) => void
+  setFocus: any
+}
+
+export default function DateRangePicker({
+  startDate,
+  endDate,
+  setValue,
+  setFocus,
+}: DateRangePickerProps) {
+  const handleChange: DatePickerProps['onChange'] = (_, dateStr) => {
+    setValue(startDate, dateStr[0])
+    setValue(endDate, dateStr[1])
+  }
+
+  return (
+    <ConfigProvider locale={globalBuddhistLocale}>
+      <DatePicker.RangePicker
+        className={styles.custom}
+        onChange={(_, dateStr) => {
+          handleChange(dayjs(), dateStr)
+        }}
+        onBlur={() => {
+          if (setFocus) {
+            setFocus(startDate)
+            setFocus(endDate)
+          }
+        }}
+      />
+    </ConfigProvider>
+  )
+}
