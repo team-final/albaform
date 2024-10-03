649a235e5af79f78585b25e8e8ceae65f89fd4ba
JuhyeokC <bos2ablues@gmail.com>
2024-10-03 17:39:14 +0900

    [feat/#76] 알바폼생성 Title 컴포넌트 추가

---
+++ src/components/FormCreate/FormCreateTitle/FormCreateTitle.tsx
@@ -0,0 +1,17 @@
+import Form from '@/components/Form/Form'
+import { useFormCreateStore } from '@/lib/stores/useFormCreate'
+
+import styles from './FormCreateTitle.module.scss'
+
+export default function FormCreateTitle() {
+  const { initialFormData } = useFormCreateStore()
+
+  return (
+    <section className={styles['form-title']}>
+      <p>알바폼 만들기</p>
+      <Form.ResetButton color={'gray'} onClick={initialFormData}>
+        작성 취소
+      </Form.ResetButton>
+    </section>
+  )
+}
