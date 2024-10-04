2f0509140139e169f3e7fda2b80a5beefccc0860
gayeong0916 <gy2039587@naver.com>
2024-10-04 23:08:22 +0900

    [feat/#140] 카카오 지도 삽입

--- src/components/FormDetails/Location/Location.tsx
+++ src/components/FormDetails/Location/Location.tsx
@@ -1,3 +1,4 @@
+import KakaoMap from '@/components/KakaoMap/KaKaoMap'
 import Toastify from '@/components/Toastify/Toastify'
 import { FormDetailsProps } from '@/lib/types/formTypes'
 import { toast } from 'react-toastify'
@@ -19,7 +20,10 @@ export default function Location({
     }
   }
 
-  const location = formDetails?.location || ''
+  const location = {
+    address: formDetails?.location || '',
+    name: formDetails?.storeName || '',
+  }
 
   return (
     <>
@@ -28,16 +32,18 @@ export default function Location({
         <h1 className={styles['location-title']}>근무 지역</h1>
         <div className={styles['location-map-container']}>
           <div className={styles['location-details']}>
-            <span className={styles['location-text']}>{location}</span>
+            <span className={styles['location-text']}>{location.address}</span>
             <button
               className={styles['location-copy']}
-              onClick={() => handleCopyClipBoard(location)}
+              onClick={() => handleCopyClipBoard(location.address)}
               disabled={!location}
             >
               복사
             </button>
           </div>
-          <section className={styles['location-map']}>이건 지도야</section>
+          <section className={styles['location-map']}>
+            <KakaoMap location={location} />
+          </section>
         </div>
       </section>
     </>
