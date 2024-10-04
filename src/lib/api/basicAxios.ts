883a90a13d0af6588219aff54896b6b5eaf10da6
gayeong0916 <gy2039587@naver.com>
2024-10-05 00:11:40 +0900

    [feat/#157] 기본 세팅

--- src/lib/api/basicAxios.ts
+++ src/lib/api/basicAxios.ts
@@ -1,9 +1,13 @@
 import axios, { AxiosInstance } from 'axios'
 
+const accessToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwic2NvcGUiOiJhY2Nlc3MiLCJpYXQiOjE3MjgwNTQzODAsImV4cCI6MTcyODA2MTU4MH0.0PlSqD2sGUJlDesTWJZgTTY9f2Hg6EhYCNYo-b1ph-U'
+
 const basicAxios: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
+    Authorization: `Bearer ${accessToken}`,
   },
 })
 
