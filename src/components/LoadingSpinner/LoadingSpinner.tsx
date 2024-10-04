623ad74df7919f321fa7ad16a8d8250959f6930f
JuhyeokC <bos2ablues@gmail.com>
2024-10-04 23:46:29 +0900

    [feat/#151] 로딩 스피너 컴포넌트 추가

---
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,16 @@
+import classNames from 'classnames'
+
+import styles from './LoadingSpinner.module.scss'
+
+export default function LoadingSpinner({ className }: { className?: string }) {
+  return (
+    <article className={classNames(styles['loading-spinner'], className)}>
+      {Array.from({ length: 5 }, (_, i) => i).map((_, i) => (
+        <div
+          key={`loading-circle-${i}`}
+          className={styles['loading-circle']}
+        ></div>
+      ))}
+    </article>
+  )
+}
