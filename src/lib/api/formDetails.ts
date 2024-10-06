6f56ddc04b730c9bb9189cdde26b7be0d827ca5b
gayeong0916 <gy2039587@naver.com>
2024-10-07 00:42:07 +0900

    [feat/#140] 정적 생성 확인

--- src/lib/api/formDetails.ts
+++ src/lib/api/formDetails.ts
@@ -1,9 +1,9 @@
-import basicAxios from './basicAxios'
+import authAxios from './authAxios'
 
 // 지원자인지 사장인지 구분하기
 export const getUsersMe = async () => {
   try {
-    const response = await basicAxios.get(`/users/me`)
+    const response = await authAxios.get(`/users/me`)
     return response.data.role
   } catch (error) {
     console.error('데이터 가져오는 중 오류 발생:', error)
@@ -14,7 +14,7 @@ export const getUsersMe = async () => {
 // 폼 목록 가져오기
 export const getlistForms = async (limit: number) => {
   try {
-    const response = await basicAxios.get(`/forms?limit=${limit}`)
+    const response = await authAxios.get(`/forms?limit=${limit}`)
     return response.data
   } catch (error) {
     console.error('데이터 가져오는 중 오류 발생:', error)
@@ -25,7 +25,7 @@ export const getlistForms = async (limit: number) => {
 // 상세폼 데이터 가져오기
 export const getFormDetails = async (formId: number) => {
   try {
-    const response = await basicAxios.get(`/forms/${formId}`)
+    const response = await authAxios.get(`/forms/${formId}`)
     return response.data
   } catch (error) {
     console.error('데이터 가져오는 중 오류 발생:', error)
@@ -36,7 +36,7 @@ export const getFormDetails = async (formId: number) => {
 // 스크랩
 export const postFormScrap = async (formId: number) => {
   try {
-    const response = await basicAxios.post(`/forms/${formId}/scrap`)
+    const response = await authAxios.post(`/forms/${formId}/scrap`)
     return response.data.isScrapped
   } catch (error) {
     console.log('데이터 생성 오류: ', error)
@@ -47,7 +47,7 @@ export const postFormScrap = async (formId: number) => {
 // 스크랩 취소
 export const deleteFormScrap = async (formId: number) => {
   try {
-    const response = await basicAxios.delete(`/forms/${formId}/scrap`)
+    const response = await authAxios.delete(`/forms/${formId}/scrap`)
     return response.data.isScrapped
   } catch (error) {
     console.log('데이터 삭제 오류: ', error)
@@ -58,7 +58,7 @@ export const deleteFormScrap = async (formId: number) => {
 // 사장이 폼 수정
 export const patchForm = async (formId: number) => {
   try {
-    const response = await basicAxios.patch(`/forms/${formId}`)
+    const response = await authAxios.patch(`/forms/${formId}`)
     return response.data
   } catch (error) {
     console.log('데이터 수정 오류: ', error)
@@ -69,7 +69,7 @@ export const patchForm = async (formId: number) => {
 // 사장이 폼 삭제
 export const deleteForm = async (formId: number) => {
   try {
-    const response = await basicAxios.delete(`/forms/${formId}`)
+    const response = await authAxios.delete(`/forms/${formId}`)
     return response
   } catch (error) {
     console.log('데이터 삭제 오류: ', error)
