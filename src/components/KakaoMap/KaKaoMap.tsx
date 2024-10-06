9bebc31ef510c17be3e15d4c4e8d0ef05ee99457
gayeong0916 <gy2039587@naver.com>
2024-10-07 01:34:26 +0900

    [feat/#140] 지도 완성

--- src/components/KakaoMap/KaKaoMap.tsx
+++ src/components/KakaoMap/KaKaoMap.tsx
@@ -47,35 +47,46 @@ const KakaoMap: React.FC<KakaoMapProps> = ({ location }) => {
           }
           const map = new Map(mapRef.current, options)
           const geocoder = new services.Geocoder()
-          geocoder.addressSearch(
-            location.address,
-            (result: any, status: any) => {
-              if (status === services.Status.OK) {
-                const coords = new LatLng(result[0].y, result[0].x)
+          if (location.address) {
+            // 주소가 있을 때만 검색 요청
+            geocoder.addressSearch(
+              location.address,
+              (result: any, status: any) => {
+                if (status === services.Status.OK) {
+                  const coords = new LatLng(result[0].y, result[0].x)
 
-                const marker = new Marker({
-                  map,
-                  position: coords,
-                })
+                  const marker = new Marker({
+                    map,
+                    position: coords,
+                  })
 
-                marker.setMap(map)
+                  marker.setMap(map)
 
-                const customOverlay = new CustomOverlay({
-                  position: coords,
-                  content: `<div class="${styles['custom-overlay']}">
+                  const customOverlay = new CustomOverlay({
+                    position: coords,
+                    content: `<div class="${styles['custom-overlay']}">
                                 <span class="${styles['overlay-marker-text']}">${location.name}</span>
                               </div>`,
-                  yAnchor: 1,
-                })
+                    yAnchor: 1,
+                  })
 
-                customOverlay.setMap(map)
-
-                map.setCenter(coords)
-              } else {
-                setMapError('주소를 찾을 수 없습니다.')
-              }
-            },
-          )
+                  customOverlay.setMap(map)
+                  map.setCenter(coords)
+                } else {
+                  // 주소 검색 실패 시 에러 메시지 설정
+                  if (status === services.Status.ZERO_RESULT) {
+                    console.log('검색 결과가 없습니다.')
+                  } else if (status === services.Status.ERROR) {
+                    console.log('주소 검색 중 오류가 발생했습니다.')
+                  } else {
+                    console.log('주소를 찾을 수 없습니다.')
+                  }
+                }
+              },
+            )
+          } else {
+            console.log('주소가 제공되지 않았습니다.') // 주소가 없는 경우 에러 메시지 설정
+          }
         } catch (error) {
           console.error('카카오맵 초기화 중 오류 발생:', error)
         }
