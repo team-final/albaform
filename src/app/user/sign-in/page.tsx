712e568c20a446cf2c56d20fa491b4cda6b7f3a2
JuhyeokC <bos2ablues@gmail.com>
2024-10-08 23:39:10 +0900

    [feat/#187] 로그인, 회원가입 페이지 디자인 적용

--- src/app/user/sign-in/page.tsx
+++ src/app/user/sign-in/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import signInSignUpStyle from '@/app/user/signInSignUp.module.scss'
+import signInSignUpStyles from '@/app/user/signInSignUp.module.scss'
 import MainButton from '@/components/Button/MainButton/MainButton'
 import Form from '@/components/Form/Form'
 import useSignIn from '@/hooks/auth/useSignIn'
@@ -8,6 +8,7 @@ import { TEST_ID_APPLICANT, TEST_ID_OWNER } from '@/lib/data/constants'
 import { emailPattern, passwordPattern } from '@/lib/data/patterns'
 import { useUserStore } from '@/lib/stores/userStore'
 import { SignInValues } from '@/lib/types/userTypes'
+import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
@@ -34,72 +35,108 @@ export default function SignInPage() {
   }, [router, user])
 
   return (
-    <>
-      <div className={signInSignUpStyle.LoginContainer}>
-        <h1>로그인</h1>
-        <p>
-          아직 계정이 없으신가요?
-          <Link
-            href={'/user/sign-up'}
-            className={signInSignUpStyle['go-to-signup']}
+    <article className={signInSignUpStyles.container}>
+      <div className={signInSignUpStyles.inner}>
+        <section className={signInSignUpStyles.header}>
+          <h1 className={signInSignUpStyles.title}>로그인</h1>
+          <div className={signInSignUpStyles.description}>
+            <p className={signInSignUpStyles['description-text']}>
+              아직 계정이 없으신가요?
+            </p>
+            <Link
+              href={'/user/sign-up'}
+              className={signInSignUpStyles['description-link']}
+            >
+              회원가입 하러 가기
+            </Link>
+          </div>
+        </section>
+
+        <section className={signInSignUpStyles.body}>
+          <Form formId={'signInForm'} onSubmit={handleSubmit}>
+            <Form.Fieldset>
+              <Form.Legend>이메일</Form.Legend>
+              <Form.Field>
+                <Form.Wrap>
+                  <Form.Input
+                    name={'email'}
+                    type={'email'}
+                    placeholder="이메일을 입력해 주세요"
+                    pattern={emailPattern}
+                    required
+                  />
+                </Form.Wrap>
+              </Form.Field>
+            </Form.Fieldset>
+
+            <Form.Fieldset>
+              <Form.Legend>비밀번호</Form.Legend>
+              <Form.Field>
+                <Form.Wrap>
+                  <Form.Input
+                    name={'password'}
+                    type={'password'}
+                    placeholder="비밀번호를 입력해 주세요"
+                    pattern={passwordPattern}
+                    required
+                  />
+                </Form.Wrap>
+              </Form.Field>
+            </Form.Fieldset>
+
+            <Form.SubmitButton
+              buttonStyle={'solid'}
+              isPending={signIn.isPending}
+            >
+              {signIn.isPending ? '로그인 중...' : '로그인'}
+            </Form.SubmitButton>
+          </Form>
+        </section>
+
+        <section className={signInSignUpStyles.sns}>
+          <div className={signInSignUpStyles['sns-title']}>
+            <p className={signInSignUpStyles['sns-title-text']}>
+              SNS 계정으로 로그인하기
+            </p>
+          </div>
+          <ul className={signInSignUpStyles['sns-list']}>
+            <li>
+              <Link href={'#'} className={signInSignUpStyles['sns-button']}>
+                <Image
+                  src={'/icons/ic-logo-google.svg'}
+                  alt={'GOOGLE 아이콘'}
+                  fill
+                />
+              </Link>
+            </li>
+            <li>
+              <Link href={'#'} className={signInSignUpStyles['sns-button']}>
+                <Image
+                  src={'/icons/ic-logo-kakao.svg'}
+                  alt={'KAKAO 아이콘'}
+                  fill
+                />
+              </Link>
+            </li>
+          </ul>
+        </section>
+
+        <section className={signInSignUpStyles.footer}>
+          <MainButton
+            buttonStyle={'outline'}
+            onClick={() => handleSignIn(TEST_ID_APPLICANT)}
           >
-            회원가입 하러 가기
-          </Link>
-        </p>
-        <Form
-          formId={'signInForm'}
-          onSubmit={handleSubmit}
-          className={signInSignUpStyle['login-form']}
-        >
-          <Form.Fieldset>
-            <Form.Legend>이메일</Form.Legend>
-            <Form.Field>
-              <Form.Wrap>
-                <Form.Input
-                  name={'email'}
-                  type={'email'}
-                  placeholder="이메일을 입력해 주세요"
-                  pattern={emailPattern}
-                  required
-                />
-              </Form.Wrap>
-            </Form.Field>
-          </Form.Fieldset>
+            지원자로 로그인
+          </MainButton>
 
-          <Form.Fieldset>
-            <Form.Legend>비밀번호</Form.Legend>
-            <Form.Field>
-              <Form.Wrap>
-                <Form.Input
-                  name={'password'}
-                  type={'password'}
-                  placeholder="비밀번호를 입력해 주세요"
-                  pattern={passwordPattern}
-                  required
-                />
-              </Form.Wrap>
-            </Form.Field>
-          </Form.Fieldset>
-
-          <Form.SubmitButton buttonStyle={'solid'} isPending={signIn.isPending}>
-            {signIn.isPending ? '로그인 중...' : '로그인'}
-          </Form.SubmitButton>
-        </Form>
-
-        <MainButton
-          buttonStyle={'outline'}
-          onClick={() => handleSignIn(TEST_ID_APPLICANT)}
-        >
-          지원자로 로그인
-        </MainButton>
-
-        <MainButton
-          buttonStyle={'outline'}
-          onClick={() => handleSignIn(TEST_ID_OWNER)}
-        >
-          사장님으로 로그인
-        </MainButton>
+          <MainButton
+            buttonStyle={'solid'}
+            onClick={() => handleSignIn(TEST_ID_OWNER)}
+          >
+            사장님으로 로그인
+          </MainButton>
+        </section>
       </div>
-    </>
+    </article>
   )
 }
--- src/app/user/sign-up/page.tsx
+++ src/app/user/sign-up/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import signInSignUpstyle from '@/app/user/signInSignUp.module.scss'
+import signInSignUpStyles from '@/app/user/signInSignUp.module.scss'
 import Form from '@/components/Form/Form'
 import useCreateUser from '@/hooks/auth/useCreateUser'
 import useSignIn from '@/hooks/auth/useSignIn'
@@ -8,6 +8,7 @@ import { emailPattern, passwordPattern } from '@/lib/data/patterns'
 import { useUserStore } from '@/lib/stores/userStore'
 import { CreateUserValues, SignUpFormValues } from '@/lib/types/userTypes'
 import { generateUniqueNickname } from '@/lib/utils/nicknameGenerator'
+import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
@@ -57,88 +58,125 @@ export default function SignUpPage() {
   }, [router, user])
 
   return (
-    <>
-      <div className={signInSignUpstyle.LoginContainer}>
-        <h1>회원가입</h1>
-        <p>
-          이미 계정이 있으신가요?
-          <Link
-            href={'/user/sign-in'}
-            className={signInSignUpstyle['go-to-signup']}
-          >
-            로그인 하러 가기
-          </Link>
-        </p>
-        <Form
-          formId={'signUpForm'}
-          onSubmit={handleSubmit}
-          className={signInSignUpstyle['login-form']}
-        >
-          <Form.Fieldset>
-            <Form.Legend>이메일</Form.Legend>
-            <Form.Field>
-              <Form.Wrap>
-                <Form.Input
-                  name={'email'}
-                  type={'email'}
-                  placeholder="이메일을 입력해 주세요"
-                  pattern={emailPattern}
-                  required
+    <article className={signInSignUpStyles.container}>
+      <div className={signInSignUpStyles.inner}>
+        <section className={signInSignUpStyles.header}>
+          <h1 className={signInSignUpStyles.title}>회원가입</h1>
+          <div className={signInSignUpStyles.description}>
+            <p className={signInSignUpStyles['description-text']}>
+              이미 계정이 있으신가요?
+            </p>
+            <Link
+              href={'/user/sign-in'}
+              className={signInSignUpStyles['description-link']}
+            >
+              로그인 하러 가기
+            </Link>
+          </div>
+        </section>
+
+        <section className={signInSignUpStyles.body}>
+          <Form formId={'signUpForm'} onSubmit={handleSubmit}>
+            <Form.Fieldset>
+              <Form.Legend>이메일</Form.Legend>
+              <Form.Field>
+                <Form.Wrap>
+                  <Form.Input
+                    name={'email'}
+                    type={'email'}
+                    placeholder="이메일을 입력해 주세요"
+                    pattern={emailPattern}
+                    required
+                  />
+                </Form.Wrap>
+              </Form.Field>
+            </Form.Fieldset>
+
+            <Form.Fieldset>
+              <Form.Legend>비밀번호</Form.Legend>
+              <Form.Field>
+                <Form.Wrap>
+                  <Form.Input
+                    name={'password'}
+                    type={'password'}
+                    placeholder="비밀번호를 입력해 주세요"
+                    pattern={passwordPattern}
+                    required
+                  />
+                </Form.Wrap>
+              </Form.Field>
+            </Form.Fieldset>
+
+            <Form.Fieldset>
+              <Form.Legend>비밀번호 확인</Form.Legend>
+              <Form.Field>
+                <Form.Wrap>
+                  <Form.Input
+                    name={'confirmPassword'}
+                    type={'password'}
+                    placeholder="비밀번호를 한 번 더 입력해 주세요"
+                    pattern={passwordPattern}
+                    required
+                  />
+                </Form.Wrap>
+              </Form.Field>
+            </Form.Fieldset>
+
+            <Form.Fieldset>
+              <Form.Legend>회원 유형</Form.Legend>
+              <div className={signInSignUpStyles['user-role-select']}>
+                <Form.Field>
+                  <Form.Label>지원자로 가입하기</Form.Label>
+                  <Form.Input
+                    name={'role'}
+                    type={'radio'}
+                    value={'APPLICANT'}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <Form.Label>사장님으로 가입하기</Form.Label>
+                  <Form.Input name={'role'} type={'radio'} value={'OWNER'} />
+                </Form.Field>
+              </div>
+            </Form.Fieldset>
+
+            <Form.SubmitButton
+              buttonStyle={'solid'}
+              isPending={createUser.isPending}
+            >
+              {createUser.isPending ? '진행 중...' : '회원 가입'}
+            </Form.SubmitButton>
+          </Form>
+        </section>
+
+        <section className={signInSignUpStyles.sns}>
+          <div className={signInSignUpStyles['sns-title']}>
+            <p className={signInSignUpStyles['sns-title-text']}>
+              SNS 계정으로 로그인하기
+            </p>
+          </div>
+          <ul className={signInSignUpStyles['sns-list']}>
+            <li>
+              <Link href={'#'} className={signInSignUpStyles['sns-button']}>
+                <Image
+                  src={'/icons/ic-logo-google.svg'}
+                  alt={'GOOGLE 아이콘'}
+                  fill
                 />
-              </Form.Wrap>
-            </Form.Field>
-          </Form.Fieldset>
-
-          <Form.Fieldset>
-            <Form.Legend>비밀번호</Form.Legend>
-            <Form.Field>
-              <Form.Wrap>
-                <Form.Input
-                  name={'password'}
-                  type={'password'}
-                  placeholder="비밀번호를 입력해 주세요"
-                  pattern={passwordPattern}
-                  required
+              </Link>
+            </li>
+            <li>
+              <Link href={'#'} className={signInSignUpStyles['sns-button']}>
+                <Image
+                  src={'/icons/ic-logo-kakao.svg'}
+                  alt={'KAKAO 아이콘'}
+                  fill
                 />
-              </Form.Wrap>
-            </Form.Field>
-          </Form.Fieldset>
-
-          <Form.Fieldset>
-            <Form.Legend>비밀번호 확인</Form.Legend>
-            <Form.Field>
-              <Form.Wrap>
-                <Form.Input
-                  name={'confirmPassword'}
-                  type={'password'}
-                  placeholder="비밀번호를 한 번 더 입력해 주세요"
-                  pattern={passwordPattern}
-                  required
-                />
-              </Form.Wrap>
-            </Form.Field>
-          </Form.Fieldset>
-
-          <Form.Fieldset>
-            <Form.Legend>회원 유형</Form.Legend>
-            <Form.Field>
-              <Form.Wrap>
-                <Form.Input name={'role'} type={'radio'} value={'APPLICANT'} />
-                지원자로 가입하기
-                <Form.Input name={'role'} type={'radio'} value={'OWNER'} />
-                사장님으로 가입하기
-              </Form.Wrap>
-            </Form.Field>
-          </Form.Fieldset>
-
-          <Form.SubmitButton
-            buttonStyle={'solid'}
-            isPending={createUser.isPending}
-          >
-            {createUser.isPending ? '진행 중...' : '회원 가입'}
-          </Form.SubmitButton>
-        </Form>
+              </Link>
+            </li>
+          </ul>
+        </section>
       </div>
-    </>
+    </article>
   )
 }
