383826bedae90bf7f3120b54a8cba02b74f2aa16
JuhyeokC <bos2ablues@gmail.com>
2024-10-09 04:15:59 +0900

    [chore/#197] 랜덤함수 교체

--- src/lib/utils/acountGenerator.ts
+++ src/lib/utils/acountGenerator.ts
@@ -1,11 +1,9 @@
-export const getRandomInt = (max: number): number => {
-  return Math.floor(Math.random() * max)
-}
+import { randomInt } from 'crypto'
 
 export const generateTestId = async (handleSubmit: any) => {
   for (let i = 0; i < 10; i++) {
     const presetRole = ['OWNER', 'APPLICANT']
-    const randomIdx = parseInt(String(getRandomInt(9999))) // 난수 생성
+    const randomIdx = parseInt(String(randomInt(0, 9999))) // 난수 생성
     await handleSubmit({
       email: `example${randomIdx}@email.com`,
       password: '00000000',
