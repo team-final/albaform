2a89c06d48630fbdf204b7fafcf6e11fc40632d5
JuhyeokC <bos2ablues@gmail.com>
2024-10-10 08:48:12 +0900

    [feat/#213] 내 정보 수정 모달 css 수정

--- src/components/Modal/UpdateInfo/Applicant/ApplicantInfoUpdate.tsx
+++ src/components/Modal/UpdateInfo/Applicant/ApplicantInfoUpdate.tsx
@@ -66,8 +66,8 @@ export default function ApplicantInfoUpdate({
       ariaHideApp={false}
       onRequestClose={onRequestClose}
       onAfterOpen={onAfterOpen}
-      overlayClassName={classNames(styles['modal-applicant-overlay'])}
-      className={classNames(styles['modal-applicant-content'])}
+      overlayClassName={classNames(styles['modal-overlay'])}
+      className={classNames(styles['modal-content'])}
     >
       <Form
         formId="applicantUpdate"
@@ -75,77 +75,78 @@ export default function ApplicantInfoUpdate({
         className={styles.inner}
         defaultValues={initialValues}
       >
-        <Form.Title>내 정보 수정</Form.Title>
-        <Form.Fieldset>
-          <div className={styles['image-preview-container']}>
-            <Form.Field className={styles['image-upload-area']}>
-              <Form.Input
-                type={'file'}
-                name={'imageUrl'}
-                onChange={handleFileChange}
+        <div>
+          <Form.Title>내 정보 수정</Form.Title>
+          <Form.Fieldset>
+            <div className={styles['image-preview-container']}>
+              <Form.Field className={styles['image-upload-area']}>
+                <Form.Input
+                  type={'file'}
+                  name={'imageUrl'}
+                  onChange={handleFileChange}
+                />
+                {previewImage ? (
+                  <Image
+                    src={previewImage}
+                    alt="Profile Preview"
+                    className={styles['image-preview']}
+                    width={100}
+                    height={100}
+                  />
+                ) : (
+                  <BasicUserProfile
+                    width={100}
+                    height={100}
+                    className={styles['image-preview']}
+                  />
+                )}
+              </Form.Field>
+              <EditIc
+                width={36}
+                heigth={36}
+                className={styles['image-basic-edit']}
               />
-              {previewImage ? (
-                <Image
-                  src={previewImage}
-                  alt="Profile Preview"
-                  className={styles['image-preview']}
-                  width={100}
-                  height={100}
-                />
-              ) : (
-                <BasicUserProfile
-                  width={100}
-                  height={100}
-                  className={styles['image-preview']}
-                />
-              )}
+            </div>
+          </Form.Fieldset>
+          <Form.Fieldset>
+            <Form.Field>
+              <Form.Legend required>이름</Form.Legend>
+              <Form.Input
+                name="name"
+                type="text"
+                required
+                placeholder="이름을 입력해주세요."
+              />
             </Form.Field>
-            <EditIc
-              width={36}
-              heigth={36}
-              className={styles['image-basic-edit']}
-            />
-          </div>
-        </Form.Fieldset>
-        <Form.Fieldset>
-          <Form.Field>
-            <Form.Legend required>이름</Form.Legend>
-            <Form.Input
-              name="name"
-              type="text"
-              required
-              placeholder="이름을 입력해주세요."
-            />
-          </Form.Field>
-        </Form.Fieldset>
-        <Form.Fieldset>
-          <Form.Field>
-            <Form.Legend required>닉네임</Form.Legend>
-            <Form.Input
-              name="nickname"
-              type="text"
-              required
-              placeholder="닉네임을 입력해주세요."
-            />
-          </Form.Field>
-        </Form.Fieldset>
-        <Form.Fieldset>
-          <Form.Field>
-            <Form.Legend required>연락처</Form.Legend>
-            <Form.Input
-              name="phoneNumber"
-              type="tel"
-              pattern={{
-                value: /^[0-9]{10,11}$/,
-                message: '잘못된 입력입니다.',
-              }}
-              placeholder="숫자만 입력해주세요."
-            />
-          </Form.Field>
-        </Form.Fieldset>
-        <div
-          className={classNames(styles['modal-applicant-content-buttongroup'])}
-        >
+          </Form.Fieldset>
+          <Form.Fieldset>
+            <Form.Field>
+              <Form.Legend required>닉네임</Form.Legend>
+              <Form.Input
+                name="nickname"
+                type="text"
+                required
+                placeholder="닉네임을 입력해주세요."
+              />
+            </Form.Field>
+          </Form.Fieldset>
+          <Form.Fieldset>
+            <Form.Field>
+              <Form.Legend required>연락처</Form.Legend>
+              <Form.Input
+                name="phoneNumber"
+                type="tel"
+                pattern={{
+                  value: /^[0-9]{10,11}$/,
+                  message: '잘못된 입력입니다.',
+                }}
+                placeholder="숫자만 입력해주세요."
+              />
+            </Form.Field>
+          </Form.Fieldset>
+        </div>
+
+        <div className={classNames(styles['modal-content-buttongroup'])}>
           <MainButton
             type="button"
             buttonStyle="outline"
@@ -155,7 +156,7 @@ export default function ApplicantInfoUpdate({
             취소
           </MainButton>
 
-          <MainButton type="submit">수정하기</MainButton>
+          <Form.SubmitButton>수정하기</Form.SubmitButton>
         </div>
       </Form>
     </ReactModal>
