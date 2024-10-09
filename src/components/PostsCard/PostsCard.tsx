0be4ef510bf1fc332538562172ce8b4c84228b4d
yoooonbong <a1056719@gmail.com>
2024-10-10 05:08:30 +0900

    [feat/#190] 타입오류수정

--- src/components/PostsCard/PostsCard.tsx
+++ src/components/PostsCard/PostsCard.tsx
@@ -18,7 +18,7 @@ interface PostsCardProps {
   recruitmentEndDate: string
   form: {
     owner: {
-      imageUrl: string[]
+      imageUrl: string
       storeName: string
       id: number
     }
