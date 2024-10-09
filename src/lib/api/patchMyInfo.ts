c00ec5d749d8efb1544f7dba1b093105d6cb102f
JuhyeokC <bos2ablues@gmail.com>
2024-10-10 01:16:59 +0900

    [feat/#206] 마이페이지 내 정보 수정 기능 개발

--- src/lib/api/patchMyInfo.ts
+++ src/lib/api/patchMyInfo.ts
@@ -2,9 +2,9 @@ import { UpdateUserValues } from '../types/userTypes'
 import handleError from '../utils/errorHandler'
 import authAxios from './authAxios'
 
-export const patchMyInfo = async ({ data }: { data: UpdateUserValues }) => {
+export const patchMyInfo = async (data: UpdateUserValues) => {
   try {
-    const response = await authAxios.patch('/users/me', { data })
+    const response = await authAxios.patch('/users/me', data)
     return response.data
   } catch (error) {
     handleError(error)
