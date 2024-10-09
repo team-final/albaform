3fb810a68e75e8ed1e27128d5f8fee4418c5e3ce
JuhyeokC <bos2ablues@gmail.com>
2024-10-10 07:25:59 +0900

    [feat/#213] EmptyContent 생성

---
+++ src/components/EmptyContent/EmptyContent.tsx
@@ -0,0 +1,77 @@
+import Image from 'next/image'
+import { ReactElement } from 'react'
+
+import styles from './EmptyContent.module.scss'
+import IconEmptyBookmark from '/public/icons/ic-empty-bookmark.svg'
+import IconEmptyComment from '/public/icons/ic-empty-comment.svg'
+import IconEmptyFormfrom from '/public/icons/ic-empty-form.svg'
+import IconInfo from '/public/icons/ic-info.svg'
+import ImagePrepareContent from '/public/images/prepare-content.jpg'
+
+type EmptyContentType =
+  | 'bookmark'
+  | 'post'
+  | 'comment'
+  | 'form'
+  | 'nodata'
+  | 'prepare'
+interface EmptyContentProps {
+  type: EmptyContentType
+}
+
+const EMPTY_CONTENT_PRESET: Record<
+  EmptyContentType,
+  { icon: ReactElement; desc: ReactElement }
+> = {
+  bookmark: {
+    icon: <IconEmptyBookmark />,
+    desc: <p>스크랩한 알바폼이 없어요.</p>,
+  },
+  post: {
+    icon: <IconEmptyComment />,
+    desc: (
+      <p>
+        작성한 게시글이 없어요.
+        <br />
+        궁금한 점, 고민 등의 게시글을 올려보세요.
+      </p>
+    ),
+  },
+  comment: { icon: <IconEmptyComment />, desc: <p>작성한 댓글이 없어요.</p> },
+  form: {
+    icon: <IconEmptyFormfrom />,
+    desc: (
+      <p>
+        등록된 알바폼이 없어요.
+        <br />
+        1분 만에 등록하고 알바를 구해보세요!
+      </p>
+    ),
+  },
+  nodata: { icon: <IconInfo />, desc: <p>등록된 데이터가 없어요.</p> },
+  prepare: {
+    icon: (
+      <Image
+        src={ImagePrepareContent}
+        alt={'콘텐츠 준비중 이미지'}
+        fill
+        priority
+        sizes={'20vw'}
+      />
+    ),
+    desc: <p>콘텐츠를 만들고 있어요.</p>,
+  },
+}
+
+export default function EmptyContent({ type = 'nodata' }: EmptyContentProps) {
+  const { icon, desc } = EMPTY_CONTENT_PRESET[type]
+
+  return (
+    <article className={styles.container}>
+      <section className={styles.inner}>
+        <div className={styles.icon}>{icon}</div>
+        <div className={styles.desc}>{desc}</div>
+      </section>
+    </article>
+  )
+}
