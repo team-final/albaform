48029c498ad3605da928b239b425d39d9babb95b
JuhyeokC <bos2ablues@gmail.com>
2024-10-10 12:31:17 +0900

    [fix/#219] 이미지 파일 경로 수정

--- next.config.mjs
+++ next.config.mjs
@@ -9,13 +9,16 @@ const nextConfig = {
   },
   pageExtensions: ['tsx', 'ts'],
   images: {
-    domains: ['albaform.netlify.app'],
     remotePatterns: [
       { protocol: 'https', hostname: '**.pixabay.co(m|.kr)' },
       { protocol: 'https', hostname: '**.freepik.co(m|.kr)' },
       {
         protocol: 'https',
         hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.netlify.app',
       },
     ],
   },
