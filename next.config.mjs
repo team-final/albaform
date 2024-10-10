1c5c4ad9f0e6f61a257df69f52d6fe33e8dab1e5
JuhyeokC <bos2ablues@gmail.com>
2024-10-10 12:19:05 +0900

    [fix/#219] next 설정 파일 수정

--- next.config.mjs
+++ next.config.mjs
@@ -9,16 +9,13 @@ const nextConfig = {
   },
   pageExtensions: ['tsx', 'ts'],
   images: {
+    domains: ['albaform.netlify.app'],
     remotePatterns: [
       { protocol: 'https', hostname: '**.pixabay.co(m|.kr)' },
       { protocol: 'https', hostname: '**.freepik.co(m|.kr)' },
       {
         protocol: 'https',
         hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'albaform.netlify.app',
       },
     ],
   },
