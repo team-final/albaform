3a44b3394b8d2962ca12c50b5451f3004726d9db
JuhyeokC <bos2ablues@gmail.com>
2024-10-19 11:56:19 +0900

    [feat/#234] 아바타 컴포넌트 개발

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,28 @@
+import classNames from 'classnames'
+import Image from 'next/image'
+
+import styles from './Avatar.module.scss'
+
+interface ProfileProps {
+  imageUrl?: string
+  name: string
+}
+
+export default function Avatar({ imageUrl, name }: ProfileProps) {
+  return (
+    <div className={styles.container}>
+      <div
+        className={classNames(styles.profile, { [styles.skeleton]: !imageUrl })}
+      >
+        <Image
+          src={imageUrl || '/icons/ic-user-profile-circle.svg'}
+          alt={imageUrl ? `${name} 프로필 이미지` : '프로필 이미지'}
+          sizes={'99vw'}
+          fill
+          priority
+        />
+      </div>
+      <p className={styles.name}>{name}</p>
+    </div>
+  )
+}
