c80b42cc958448b8a76cc85f66012fcc1ccb2728
JuhyeokC <bos2ablues@gmail.com>
2024-10-19 11:54:53 +0900

    [feat/#234] 알바토크 상세보기 관련 API 추가

--- src/lib/api/albatalk.ts
+++ src/lib/api/albatalk.ts
@@ -5,20 +5,69 @@ import { AxiosResponse } from 'axios'
 
 import authAxios from './authAxios'
 
-export const addtalk = async (requestBody: string): Promise<AxiosResponse> => {
+const API_URL = 'posts'
+
+export const postAlbatalk = async (
+  requestBody: string,
+): Promise<AxiosResponse> => {
   try {
-    return await authAxios.post('posts', requestBody)
+    return await authAxios.post(`${API_URL}`, requestBody)
   } catch (error) {
     console.error('알바토크 만들기 중 오류 발생:', error)
     throw error
   }
 }
 
-export const getAlbatalk = async (postId: string) => {
+export const patchAlbatalk = async (
+  postId: number,
+  requestBody: string,
+): Promise<AxiosResponse> => {
   try {
-    return await authAxios.get(`posts/${Number(postId)}`)
+    return await authAxios.patch(`${API_URL}/${postId}`, requestBody)
+  } catch (error) {
+    console.error('알바토크 수정 중 오류 발생:', error)
+    throw error
+  }
+}
+
+export const deleteAlbatalk = async (
+  postId: number,
+): Promise<AxiosResponse> => {
+  try {
+    return await authAxios.delete(`${API_URL}/${postId}`)
+  } catch (error) {
+    console.error('알바토크 삭제 중 오류 발생:', error)
+    throw error
+  }
+}
+
+export const getAlbatalk = async (postId: number) => {
+  try {
+    return await authAxios.get(`${API_URL}/${postId}`)
   } catch (error) {
     console.error('알바토크 불러오기 중 오류 발생:', error)
+    throw error
+  }
+}
+
+export const postAlbatalkLike = async (
+  postId: number,
+): Promise<AxiosResponse> => {
+  try {
+    return await authAxios.post(`${API_URL}/${postId}/like`)
+  } catch (error) {
+    console.error('알바토크 좋아요 중 오류 발생:', error)
+    throw error
+  }
+}
+
+export const deleteAlbatalkLike = async (
+  postId: number,
+): Promise<AxiosResponse> => {
+  try {
+    return await authAxios.delete(`${API_URL}/${postId}/like`)
+  } catch (error) {
+    console.error('알바토크 좋아요 취소 중 오류 발생:', error)
     throw error
   }
 }
