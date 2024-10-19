d9645cdc257296d21e01eb319dbdb70a9322ec66
JuhyeokC <bos2ablues@gmail.com>
2024-10-19 11:56:33 +0900

    [feat/#234] 아이콘텍스트 컴포넌트 개발

---
+++ src/components/IconText/IconText.tsx
@@ -0,0 +1,44 @@
+import { ChildrenProps, ClickProps } from '@/lib/types/types'
+import Image from 'next/image'
+
+import styles from './IconText.module.scss'
+
+interface IconTextProps extends ChildrenProps, ClickProps {
+  disabled?: boolean
+}
+
+export default function IconText({
+  children,
+  onClick,
+  disabled,
+}: IconTextProps) {
+  if (onClick) {
+    return (
+      <button
+        type={'button'}
+        className={styles.container}
+        onClick={onClick}
+        disabled={disabled}
+      >
+        {children}
+      </button>
+    )
+  } else {
+    return <div className={styles.container}>{children}</div>
+  }
+}
+
+function Icon({ src, alt }: { src: string; alt: string }) {
+  return (
+    <div className={styles.icon}>
+      <Image src={src} alt={alt} fill sizes={'99vw'} />
+    </div>
+  )
+}
+
+function Text({ text }: { text: string | number }) {
+  return <p className={styles.text}>{text}</p>
+}
+
+IconText.Icon = Icon
+IconText.Text = Text
