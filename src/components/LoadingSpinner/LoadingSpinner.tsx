c6f9add099a13c69cb398007b6eaa55bdd92d11d
JuhyeokC <bos2ablues@gmail.com>
2024-10-24 16:54:14 +0900

    [feat/#258] LoadingSpiner 크기 수정

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -5,12 +5,14 @@ import styles from './LoadingSpinner.module.scss'
 export default function LoadingSpinner({ className }: { className?: string }) {
   return (
     <article className={classNames(styles['loading-spinner'], className)}>
-      {Array.from({ length: 5 }, (_, i) => i).map((_, i) => (
-        <div
-          key={`loading-circle-${i}`}
-          className={styles['loading-circle']}
-        ></div>
-      ))}
+      <div className={styles['loading-inner']}>
+        {Array.from({ length: 5 }, (_, i) => i).map((_, i) => (
+          <div
+            key={`loading-circle-${i}`}
+            className={styles['loading-circle']}
+          ></div>
+        ))}
+      </div>
     </article>
   )
 }
