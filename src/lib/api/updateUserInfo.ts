0159b1278eeed2eca92d696b34dc0f034e922d74
JiinSeok <seokjiin1073@gmail.com>
2024-10-31 19:19:55 +0900

    [refactor/#304] 함수 이름 컨벤션에 맞게 변경 https://cloud.google.com/apis/design/naming_convention?hl=ko

--- src/lib/api/updateUserInfo.ts
+++ src/lib/api/updateUserInfo.ts
@@ -3,7 +3,7 @@ import { UpdateUserValues } from '@/lib/types/userTypes'
 import handleError from '../utils/errorHandler'
 import authAxios from './authAxios'
 
-export const patchMyInfo = async (data: UpdateUserValues) => {
+export const updateUserInfo = async (data: UpdateUserValues) => {
   try {
     const response = await authAxios.patch('/users/me', data)
     return response.data
