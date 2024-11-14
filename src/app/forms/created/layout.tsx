2d5696fc178a1d86cf8896fd1e201cd4582b245d
JiinSeok <126224323+jiinseok@users.noreply.github.com>
2024-11-14 21:33:18 +0900

    [refactor/#323] 루트 레이아웃과 중복 파일 삭제

--- src/app/forms/created/layout.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import DefualtQueryProvider from '@/lib/queries/DefaultQueryProvider'
-import { LayoutProps } from '@/lib/types/types'
-
-export default function FormLayout({ children }: LayoutProps) {
-  return <DefualtQueryProvider>{children}</DefualtQueryProvider>
-}
