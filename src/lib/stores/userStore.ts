0878a5c9d0ca9be5990bdc0d59b186e913e1aa09
JiinSeok <126224323+jiinseok@users.noreply.github.com>
2024-11-14 21:42:57 +0900

    [refactor/#323] 유저 정보 저장소 세션 스토리지에서 로컬 스토리지로 수정

--- src/lib/stores/userStore.ts
+++ src/lib/stores/userStore.ts
@@ -1,44 +1,24 @@
-import { OauthService, User } from '@/lib/types/userTypes'
+import { AuthProvider, User } from '@/lib/types/userTypes'
 import { create } from 'zustand'
-import { PersistStorage, persist } from 'zustand/middleware'
+import { persist } from 'zustand/middleware'
 
 export interface UserStore {
-  user: User | undefined
-  setUser: (nowUser: User | undefined) => void
-  authService: OauthService | undefined
-  setAuthService: (nowService: OauthService | undefined) => void
-}
-
-const customSessionStorage: PersistStorage<UserStore> = {
-  getItem: (name) => {
-    const item = sessionStorage.getItem(name)
-    return item ? JSON.parse(item) : null // JSON 파싱
-  },
-  setItem: (name, value) => {
-    sessionStorage.setItem(name, JSON.stringify(value)) // JSON 문자열화
-  },
-  removeItem: (name) => {
-    sessionStorage.removeItem(name)
-  },
+  user?: User
+  authService?: AuthProvider
+  setUser: (user?: User) => void
+  setAuthService: (authService?: AuthProvider) => void
 }
 
 export const useUserStore = create(
   persist<UserStore>(
     (set) => ({
       user: undefined,
-      setUser: (nowUser: User | undefined) => set({ user: nowUser }),
       authService: undefined,
-      setAuthService: (nowService: OauthService | undefined) =>
-        set({ authService: nowService }),
+      setUser: (newUser) => set({ user: newUser }),
+      setAuthService: (newService) => set({ authService: newService }),
     }),
     {
-      name: 'albaform-user-storage',
-      storage: customSessionStorage,
-      onRehydrateStorage: (state) => {
-        if (!state) {
-          console.warn('스토리지 재구성이 실패했습니다.')
-        }
-      },
+      name: 'albaform-user',
     },
   ),
 )
