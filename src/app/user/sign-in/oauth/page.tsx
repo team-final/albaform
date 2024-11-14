a08430daadd79f228af6ebb1089496ab330c9dc6
JiinSeok <126224323+jiinseok@users.noreply.github.com>
2024-11-14 21:38:40 +0900

    [refactor/#323] 로그인 함수, 회원가입 함수 공통화

--- src/app/user/sign-in/oauth/page.tsx
+++ src/app/user/sign-in/oauth/page.tsx
@@ -1,34 +1,32 @@
 'use client'
 
 import LoadingSpinner from '@/components/LoadingSpinner/LoadingSpinner'
-import useOauth from '@/hooks/auth/useOauth'
+import useSignIn from '@/hooks/auth/useSignIn'
 import useHydration from '@/hooks/useHydration'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useCallback, useEffect } from 'react'
 
 export default function KakaoSignInHandler() {
   const isHydrated = useHydration()
-  const { oauthSignIn } = useOauth()
+  const { oauthSignIn } = useSignIn()
   const responseParams = useSearchParams()
   const authorizeCode = responseParams.get('code') ?? ''
   const router = useRouter()
 
-  const hanldeKakaoSignIn = useCallback(async () => {
+  const handleKakaoSignIn = useCallback(async () => {
     console.log('🚀 ~ KakaoSignInHandler ~ authorizeCode:', authorizeCode)
-    const res = await oauthSignIn.mutateAsync({
+    await oauthSignIn.mutateAsync({
       provider: 'kakao',
       redirectUri: process.env.NEXT_PUBLIC_KAKAO_SIGNIN_REDIRECT_URI,
-      token: authorizeCode,
+      providerToken: authorizeCode,
     })
     router.replace('/')
-    console.log('🚀 ~ hanldeKakaoSignIn ~ res:', res)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   useEffect(() => {
-    console.log('🚀 ~ useEffect ~ isHydrated:', isHydrated)
-    if (isHydrated) hanldeKakaoSignIn()
-  }, [isHydrated, hanldeKakaoSignIn])
+    if (isHydrated) handleKakaoSignIn().then()
+  }, [isHydrated, handleKakaoSignIn])
 
   return <LoadingSpinner full />
 }
