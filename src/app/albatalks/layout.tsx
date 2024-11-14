bbcf964ea52e96a2c95ca75dc9c96fddf8f26666
JiinSeok <126224323+jiinseok@users.noreply.github.com>
2024-11-14 21:33:18 +0900

    [refactor/#323] 루트 레이아웃과 중복 파일 삭제

--- src/app/albatalks/layout.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import DefaultQueryProvider from '@/lib/queries/DefaultQueryProvider'
-import { ChildrenProps } from '@/lib/types/types'
-
-export default function AlbatalksLayout({ children }: ChildrenProps) {
-  return <DefaultQueryProvider>{children}</DefaultQueryProvider>
-}
--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,7 +1,4 @@
 import { ReactNode } from 'react'
-import { FieldValues, SubmitHandler } from 'react-hook-form'
-
-import { WorkDaysType } from './formTypes'
 
 export interface CustomMessage {
   title?: string
@@ -23,91 +20,21 @@ export interface ClickProps {
   onClick?: () => void
 }
 
-export interface LayoutProps {
+export type LayoutProps = Readonly<{
+  children: ReactNode
+}>
+
+export interface ChildrenProps {
   children: ReactNode
 }
 
-export interface ChildrenProps {
+export interface ComponentProps {
   children?: ReactNode
-}
-
-export interface ComponentProps extends ChildrenProps {
   className?: string
 }
 
-export interface FormProps extends ComponentProps {
-  formId: string
-  onSubmit: SubmitHandler<FieldValues>
-  initialValues?: Record<string, any>
-  defaultValues?: FieldValues
-}
-
-export interface FormFieldProps extends ComponentProps {
-  htmlFor?: string
-  isInline?: boolean
-  hidden?: boolean
-}
-
-export interface FormLegendProps extends ComponentProps {
-  required?: boolean
-}
-
-export interface InputProps extends ComponentProps {
-  name: string
-  type?: HTMLInputElement['type']
-  required?: boolean
-  checked?: boolean
-  disabled?: boolean
-  placeholder?: string
-  autoComplete?: string
-  minLength?: number
-  maxLength?: number
-  pattern?: {
-    value: RegExp
-    message: string
-  }
-  value?: any
-  validate?: any
-  initialValues?: string
-  min?: number
-  max?: number
-  step?: number
-  onClick?: (event?: any) => void
-  onChange?: (event?: any) => void
-  workDaysValue?: WorkDaysType
-}
-
-export interface FormDetailsProps {
-  updatedAt?: string
-  createdAt?: string
-  preferred?: string
-  age?: string
-  education?: string
-  gender?: string
-  numberOfPositions?: number
-  isPublic?: boolean
-  hourlyWage?: number
-  workDays?: string[]
-  workEndTime?: string
-  workStartTime?: string
-  workEndDate?: string
-  workStartDate?: string
-  location?: string
-  imageUrls?: string[]
-  recruitmentEndDate?: string
-  recruitmentStartDate?: string
-  description?: string
-  title?: string
-  ownerId?: number
-  id?: number
-  scrapCount?: number
-  applyCount?: number
-  isScrapped?: boolean
-  phoneNumber?: string
-  storePhoneNumber?: string
-}
-
 export type MyContentMenuType = 'posts' | 'comments' | 'scrap'
+
 export interface MyContentMenu {
   value: MyContentMenuType
   label: string
@@ -118,6 +45,7 @@ export type ScrapListSortConditionType =
   | 'highestWage'
   | 'mostApplied'
   | 'mostScrapped'
+
 export interface ScrapListSortCondition {
   value: ScrapListSortConditionType
   label: string
