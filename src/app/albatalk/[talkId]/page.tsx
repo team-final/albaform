5c4606a9e3dc6c754c425e6ce6de3d5c7e66ab64
JiinSeok <126224323+jiinseok@users.noreply.github.com>
2024-11-15 15:17:56 +0900

    [feat/#332] 알바토크 게시글 로그인 안 하면 로그인 페이지로 리다이렉트

--- src/app/albatalk/[talkId]/page.tsx
+++ src/app/albatalk/[talkId]/page.tsx
@@ -1,13 +1,16 @@
-'use client'
-
 import Albatalk from '@/components/Albatalk/Albatalk/Albatalk'
 import Comment from '@/components/Comment/Comment'
 import { Params } from '@/lib/types/types'
+import { cookies } from 'next/headers'
 
 import styles from './page.module.scss'
 
-export default function AlbatalkPage({ params }: Params) {
+export default async function AlbatalkPage({ params }: Params) {
+  const accessToken = cookies().get('accessToken')?.value
   const { talkId } = params
+  if (!accessToken) {
+    return <p>Redirecting...</p> // 인증이 없다면 미들웨어가 리다이렉트
+  }
 
   return (
     <main className={styles.container}>
